58b3bf0c545c0a24ab6c59cc8943cc7c9c8e31cf
gspinaci <gianmarco.spinaci@gmail.com>
2024-06-19 16:11:23 +0200

    create graph
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- upload/script.py
+++ upload/script.py
@@ -1,10 +1,10 @@
-
 import click
 import os
 import docx
 import shutil
 import re
 import pandas as pd
+import datetime
 from rdflib import Graph, URIRef, namespace, Namespace, Literal
 
 REGEX_PAGE = r'\[p\.\s(\d+)\]'
@@ -13,6 +13,22 @@ REGEX_SEQUENCE = r'^\d+_\d{3}$'
 KEY_PARAGRAPHS = 'paragraphs'
 KEY_SEQUENCE = 'sequence'
 
+BASE_URI = 'https://belle-greene.com/'
+RESOURCE = f'{BASE_URI}resource/'
+
+PLATFORM = Namespace('http://www.researchspace.org/resource/system/')
+CUSTOM = Namespace(f'{RESOURCE}custom/')
+LDP = Namespace('http://www.w3.org/ns/ldp#')
+CRM = Namespace('http://www.cidoc-crm.org/cidoc-crm/')
+DPUB_ANNOTATION = Namespace(f'{BASE_URI}document/annotation-schema/')
+CRMDIG = Namespace('http://www.ics.forth.gr/isl/CRMdig/')
+PROV = Namespace('http://www.w3.org/ns/prov#')
+MB_DIARIES = Namespace('https://mbdiaries.itatti.harvard.edu/ontology/')
+RDF = namespace.RDF
+RDFS = namespace.RDFS
+XSD = namespace.XSD
+
+
 def _extract_paragraphs_from_docx(docx_path):
     try:
         doc = docx.Document(docx_path)
@@ -20,8 +36,6 @@ def _extract_paragraphs_from_docx(docx_path):
         return paragraphs
     except Exception as e:
         print(str(e))
-
-
 
 def _get_letters(path):
     try:
@@ -103,17 +117,78 @@ def _restore(path):
         shutil.rmtree(path)
     os.mkdir(path)
 
-def _create_graph(para):
+
+def _create_graph_letter(letter_number, sequences, img_path, output_path):
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+            
+    for page, sequence in sequences.items():
+        _create_graph(sequence, page, letter_number, img_path)
+        g = _create_graph(sequence, page, letter_number, img_path)
+        g.serialize(os.path.join(output_path, f'{letter_number}_{page}.ttl'), format='turtle')
+
+
+def _create_graph(para, page_number, letter_number, img_path):
     g = Graph()
     
-    PAGE_NODE = Namespace("http://example.org/page/")
+    LETTER_NODE = URIRef(f"https://belle-greene.com/resource/letter/{letter_number}")
+    PAGE_NODE = URIRef(f"https://belle-greene.com/resource/letter/{letter_number}/page/{page_number}")
+    PAGE_NODE_DOCUMENT = URIRef(f"https://belle-greene.com/resource/letter/{letter_number}/document/{page_number}")
+    
+    try:
+        
+        g.add((PLATFORM.fileContainer, LDP.contains, PAGE_NODE_DOCUMENT))
+        g.add((PAGE_NODE_DOCUMENT, RDF.type, PLATFORM.File))
+        g.add((PAGE_NODE_DOCUMENT, RDF.type, LDP.Resource))
+        g.add((PAGE_NODE_DOCUMENT, RDF.type, URIRef(
+            f'{BASE_URI}ontology/Document')))
+        g.add((PAGE_NODE_DOCUMENT, RDFS.label, Literal(
+            f'LDP Container of document file of {letter_number}_{page_number}.html', datatype=XSD.string)))
+        g.add((PAGE_NODE_DOCUMENT, PLATFORM.fileContext, URIRef(
+            'http://www.researchspace.org/resource/TextDocuments')))
+        g.add((PAGE_NODE_DOCUMENT, PLATFORM.fileName, Literal(
+            f'Letter {letter_number}_{page_number}.html', datatype=XSD.string)))
+        g.add((PAGE_NODE_DOCUMENT, PLATFORM.mediaType,
+            Literal('form-data', datatype=XSD.string)))
+        g.add((PAGE_NODE_DOCUMENT, PROV.generatedAtTime, Literal(
+            datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), datatype=XSD.dateTime)))
+        g.add((PAGE_NODE_DOCUMENT, PROV.wasAttributedTo, URIRef(
+            'http://www.researchspace.org/resource/admin')))
+        g.add((PAGE_NODE, CRM.P129i_is_subject_of, PAGE_NODE_DOCUMENT))
+        
+        # Visual representation
+        IMAGE_NODE = URIRef(f'{img_path}{para[KEY_SEQUENCE]}.jpg')
+        g.add((IMAGE_NODE, RDF.type, CRM.E38_Image))
+        g.add((IMAGE_NODE, RDF.type, URIRef(
+            'http://www.researchspace.org/ontology/EX_Digital_Image')))
+        g.add((PAGE_NODE, CRM.P183i_has_representation, IMAGE_NODE))
+
+        g.add((PAGE_NODE, RDF.type, CUSTOM['Page_Letter']))
+        # g.add((PAGE_NODE, RDF.type, CRM['E22_Man-Made_Object']))
+        g.add((PAGE_NODE, RDFS.label, Literal(page_number, datatype=XSD.string)))
+        g.add((PAGE_NODE, CUSTOM['index'], Literal(page_number, datatype=XSD.string)))
+        g.add((PAGE_NODE, CUSTOM['part_of'], LETTER_NODE))
+        
+        g.namespace_manager.bind('Platform', PLATFORM, override=True, replace=True)
+        g.namespace_manager.bind('custom', CUSTOM, override=True, replace=True)
+        g.namespace_manager.bind('crm', CRM, override=True, replace=True)
+        g.namespace_manager.bind('crmdig', CRMDIG, override=True, replace=True)
+        g.namespace_manager.bind('ldp', LDP, override=True, replace=True)
+        g.namespace_manager.bind('prov', PROV, override=True, replace=True)
+
+    except Exception as e:
+        print(str(e))
+
+    return g
 
 @click.command()
 @click.option('-u', 'exec_upload', is_flag=True, help="Execute the upload", default=False)
 @click.option('-P', 'prune', is_flag=True, help="Prune output folder", default=False)
 @click.option('-direct', 'direct_path', type=click.Path())
 @click.option('-l', 'limit', type=int, default=-1)
-def exec(exec_upload, prune, direct_path, limit):
+@click.option('-I', 'img_path', type=str, default='https://iiif-bucket.s3.eu-west-1.amazonaws.com/bellegreene500/')
+def exec(exec_upload, prune, direct_path, limit, img_path):
     cur_path = os.path.dirname(os.path.realpath(__file__))
     
     input_path = os.path.join(cur_path, "input")
@@ -168,7 +243,10 @@ def exec(exec_upload, prune, direct_path, limit):
             print(f'Number of pages in Letter {key} is not equal to the number of pages in the parsed letter')
 
 
-    # generate graphs
+    # generate graphs for each letter page
+    for key, parsed_letter in parsed_letters.items():
+        _create_graph_letter(key, parsed_letter, img_path, output_path)
+
     
         
     # upload graphs
